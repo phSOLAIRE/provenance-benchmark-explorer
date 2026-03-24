635f71686366dcb92f3eb8c993064135b6bfe4bf
phSOLAIRE <jonas.rieling@outlook.com>
2026-03-24 10:30:26 +0100

    open source labels folder

--- src/provenance_explorer/registry/registry_all.py
+++ src/provenance_explorer/registry/registry_all.py
@@ -12,6 +12,9 @@ WORK = Path(os.getenv("WORK", "")) # present on unified HPC system
 DATA_RAW = Path(os.getenv("WS_RAW_PATH", ""))
 DATA_NE04J = Path(os.getenv("WS_NEO4J_PATH", ""))
 
+# place to store open source labels
+DARPA_LABEL_PATH = WORK / "darpa-labels"
+
 # Cache for expesive data retrieval for plots
 CACHE_ROOT = WORK / "provenance-explorer-cache"
  
